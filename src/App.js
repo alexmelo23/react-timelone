107fa2b3dec095cd6d978c22c0a2fc620e110c7b
alexmelo23 <alexmalin23@gmail.com>
2021-03-10 17:14:53 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import TimeLine from "../src/timeLine";
+export default function App() {
+  return (
+    <div>
+      <h1>Hello timeLine!</h1>
+      <TimeLine />
+    </div>
+  );
+}
---
+++ src/timeLine/index.js
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+import Marks from "./marks";
+
+const TimeLine = () => {
+  return (
+    <div id="timeLine">
+      <Marks />
+    </div>
+  );
+};
+
+export default TimeLine;
